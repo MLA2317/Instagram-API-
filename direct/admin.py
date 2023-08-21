22b7697699d09217d1ccdaa109aa00d216d613cf
Lazizbek <102922781+MLA2317@users.noreply.github.com>
2023-08-21 18:10:05 +0500

    DirectMessage model done

--- direct/admin.py
+++ direct/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import DirectMessage
 
-admin.site.register(DirectMessage)
+@admin.register(DirectMessage)
+class DirectMessageAdmin(admin.ModelAdmin):
+    list_editable = ['is_read']
+    list_display = ['id', 'sender', 'receiver', 'file', 'is_read', 'created_date']
--- direct/models.py
+++ direct/models.py
@@ -4,14 +4,19 @@ from post.models import Post
 
 
 class DirectMessage(models.Model):
-    sender = models.ForeignKey(Follow, on_delete=models.CASCADE, null=True)
-    reciever = models.ForeignKey(Follow, on_delete=models.CASCADE, null=True, related_name='reciever')
+    sender = models.ForeignKey(Account, on_delete=models.CASCADE, null=True, related_name='from_user')
+    receiver = models.ForeignKey(Account, on_delete=models.CASCADE, null=True, related_name='to_user')
     message = models.TextField()
-    file = models.FileField(upload_to='media/', null=True, blank=True)
+    file = models.FileField(upload_to='direct_media/', null=True, blank=True)
     is_read = models.BooleanField(blank=False)
     post_id = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['created_date']
+        verbose_name = 'Direct Message'
+        verbose_name_plural = 'Direct Messages'
 
-
+    def __str__(self):
+        return f'{self.sender} - {self.receiver}'
 
