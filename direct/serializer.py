db55aacb193aafc559eb8fd6a34691d5d5206590
Lazizbek <102922781+MLA2317@users.noreply.github.com>
2023-08-22 15:31:02 +0500

    Direct Done

--- direct/serializer.py
+++ direct/serializer.py
@@ -1,2 +1,30 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from .models import DirectMessage
+from account.serializer import ProfilesSerializer
+
+
+class DirectMessageSerializer(serializers.ModelSerializer):
+    sender_name = serializers.SerializerMethodField()
+    receiver_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = DirectMessage
+        fields = ('id', 'sender_name', 'receiver_name', 'message', 'file', 'is_read', 'created_date')
+        extra_kwargs = {
+            'post_id': {'required': False}
+        }
+
+    def get_sender_name(self, obj):
+        return obj.sender.username
+
+    def get_receiver_name(self, obj):
+        return obj.receiver.username
+
+    def validate_sender(self, value):
+        request_user = self.context['request'].user
+        print('rr', request_user)
+        if request_user != value:
+            raise ValidationError("You can't send a message on behalf of another user")
+        return value
+
